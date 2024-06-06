1e639f5ff8c22bca0a6202227b309b3637799520
hady <osmanhady73@gmail.com>
2024-06-06 06:44:40 +0300

    configure cors

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const mongoose = require("mongoose");
 const path = require("path");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
