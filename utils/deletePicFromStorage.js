3d64d7750fafdf9c63a2495b5e0025e0747cce2f
hady <osmanhady73@gmail.com>
2024-06-12 11:19:09 +0300

    fix small bug

--- utils/deletePicFromStorage.js
+++ utils/deletePicFromStorage.js
@@ -3,6 +3,9 @@ const { ref, deleteObject } = require("firebase/storage");
 module.exports = async (picUrl) => {
   try {
     const path = decodeURIComponent(picUrl.split("/o/")[1].split("?")[0]);
+    if (path === "images/anonymous_user.webp") {
+      return false;
+    }
     const storageRef = ref(firebaseStorage, path);
     const result = await deleteObject(storageRef);
     console.log("result ===>", result);
